088eac380c9cdcbc05147d7dfb6cd5d5ae170906
Nikhil Kumar <airmailtonikhil@gmail.com>
2021-06-11 11:29:55 +0530

    I am adding sound effect for analog clock

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+var popUpbtn = document.querySelector(".pop-up button");
 var clock = document.querySelector(".clock");
 var hourTail = document.querySelector(".hour-tail");
 var minuteTail = document.querySelector(".minute-tail");
 var secondTail = document.querySelector(".second-tail");
 var digitelbtn = document.querySelector(".digitel");
 var analogbtn = document.querySelector(".analog");
+var analogClock_sound = new Audio("clock_tick.mp3");
 var date = new Date();
 var hour = date.getHours();
 var min = date.getMinutes();
 var sec = date.getSeconds();
 var ampm = (hour < 12 ? "AM":"PM");
+var is_clock_analog = true;
 var hourDegree = hour/12 * 360 + min/60 * 360/12 - 90 ;
 var minuteDegree = min/60 * 360 + sec/60 * 360/60 - 90;
 var secondDegree = sec/60 * 360 - 90;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
+
+
+// For removing pop-up window which appears at beggining
+// Also it is used to enable javascipt sound by interacting with website
+popUpbtn.addEventListener("click",()=>{
+    document.querySelector(".pop-up").style.display = "none";
+    document.querySelector(".analog-clock").style.display = "initial";
+});
 
 function setClock(){
     date = new Date();
@@ -29,15 +40,22 @@ function setClock(){
     hour = (hour < 10 ? "0" : "") + hour;
     min = (min < 10 ? "0" : "") + min;
     sec = (sec < 10 ? "0" : "") + sec;
-    clock.innerHTML = hour+":"+min+":"+sec+" "+ampm;
+    
 
+    // This is checkpoint which checks analog clock is selected or digitel clock
+    if(!is_clock_analog)
+    {
+        clock.innerHTML = hour+":"+min+":"+sec+" "+ampm;
+        return;
+    }
     // Here analog clock is set
     hourDegree = hour/12 * 360 + min/60 * 360/12 - 90  ;
     minuteDegree = min/60 * 360 + sec/60 * 360/60 - 90 ;
-    secondDegree = sec/60 * 360 - 90 ;
+    secondDegree = Math.floor(sec/60 * 360) - 90 ;
     hourTail.style.transform = 'rotate('+ hourDegree +'deg)';
     minuteTail.style.transform = 'rotate('+ minuteDegree +'deg)';
     secondTail.style.transform = 'rotate('+ secondDegree +'deg)';
+    analogClock_sound.play();
 }
 
 
@@ -46,20 +64,24 @@ function setClock(){
 async function updateval(){
     while(true){
         setClock();
-        await sleep(500);
+        await sleep(1000);
     }
    
 }
 
 updateval();
 
+// For selecting digitel clock
 digitelbtn.addEventListener("click",function(){
     document.querySelector(".clock").style.display = "flex";
     document.querySelector(".analog-clock").style.display = "none";
+    is_clock_analog = false;
 });
+// For selecting analog clock
 analogbtn.addEventListener("click",function(){
     document.querySelector(".clock").style.display = "none";
     document.querySelector(".analog-clock").style.display = "initial";
+    is_clock_analog = true;
 });
 
 
